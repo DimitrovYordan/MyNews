ce40e8d705693256ba9547e827c4cb86acc1b4ec
Yordan <y.dimitrov84@gmail.com>
2025-09-05 10:26:04 +0300

    Add loading and styles, show all sections.

--- MyNews-ui/src/app/services/news.service.ts
+++ MyNews-ui/src/app/services/news.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { NewsItem } from "../interfaces/news-item";
+import { SectionWithNews } from "../interfaces/section-with-news";
 
 @Injectable({
     providedIn: 'root'
@@ -11,17 +12,15 @@ import { NewsItem } from "../interfaces/news-item";
 export class NewsService {
     private apiUrl = 'http://localhost:5271/api/news';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getAllNews(): Observable<NewsItem[]> {
         return this.http.get<NewsItem[]>(this.apiUrl);
     }
 
-    getNewsBySections(sectionIds: number[]): Observable<NewsItem[]> {
-        let params = new HttpParams();
-        sectionIds.forEach(id => {
-            params = params.append('sectionIds', id.toString());
+    getNewsBySections(sectionIds: number[]): Observable<SectionWithNews[]> {
+        return this.http.post<SectionWithNews[]>(`${this.apiUrl}/by-sections`, sectionIds, {
+            headers: { 'Content-Type': 'application/json' }
         });
-        return this.http.get<NewsItem[]>(this.apiUrl, { params });
     }
 }
