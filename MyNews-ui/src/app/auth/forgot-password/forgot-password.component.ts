f40e51a4712f3b332a5c864d035d8edb33c2802b
Yordan <y.dimitrov84@gmail.com>
2025-09-07 20:25:02 +0300

    Add Log in,Sign up,Forgot password ui

---
+++ MyNews-ui/src/app/auth/forgot-password/forgot-password.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
+})
+export class ForgotPasswordComponent {
+  @Output() close = new EventEmitter<void>();
+  @Output() backToSignup = new EventEmitter<void>();
+
+  forgotPasswordForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.forgotPasswordForm.valid) {
+      const email = this.forgotPasswordForm.value.email;
+
+      this.authService.forgotPassword(email).subscribe({
+        next: () => alert('Password reset email sent.'),
+        error: () => alert('Failed to send password email.'),
+      });
+    }
+  }
+
+  goBack() {
+    this.backToSignup.emit();
+  }
+}
