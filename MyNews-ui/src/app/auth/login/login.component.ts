25b0cd7f7605240e518528c1bd009c5670382c24
Yordan <y.dimitrov84@gmail.com>
2025-09-09 22:25:28 +0300

    Add eye icon to input field, error message in log in and styles.

--- MyNews-ui/src/app/auth/login/login.component.ts
+++ MyNews-ui/src/app/auth/login/login.component.ts
@@ -13,8 +13,14 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent {
   @Output() forgotPassword = new EventEmitter<void>();
-  
+  @Output() close = new EventEmitter<void>();
+  @Output() loginSuccess = new EventEmitter<void>();
+
   loginForm: FormGroup;
+
+  errorMessage: string | null = null;
+  loading = false;
+  showPassword: boolean = false;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
@@ -29,8 +35,15 @@ export class LoginComponent {
         next: (res) => {
           localStorage.setItem('token', res.token);
           console.log('Logged in!');
+          this.close.emit();
+          this.loginSuccess.emit();
+          this.loading = false;
         },
-        error: (err: any) => console.error(err)        
+        error: (err: any) => {
+          console.error(err);
+          this.loading = false;
+          this.errorMessage = err.error || 'Invalid email or password.';
+        }
       });
     }
   }
--- MyNews-ui/src/app/services/auth.service.ts
+++ MyNews-ui/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, tap } from "rxjs";
 
 import { AuthRequest } from "../interfaces/auth-request";
 import { SignupData } from "../interfaces/signup";
@@ -11,15 +11,31 @@ import { SignupData } from "../interfaces/signup";
 })
 export class AuthService {
     private apiUrl = 'http://localhost:5271/api/auth';
+    private tokenKey = 'auth_token';
 
-    constructor(private http: HttpClient) { }
+    public isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+    constructor(private http: HttpClient) {
+        const token = localStorage.getItem(this.tokenKey);
+        this.isLoggedIn$.next(!!token);
+    }
 
     login(credentials: AuthRequest): Observable<any> {
-        return this.http.post(`${this.apiUrl}/login`, credentials);
+        return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
+            tap((res: any) => {
+                localStorage.setItem(this.tokenKey, res.token);
+                this.isLoggedIn$.next(true);
+            })
+        );
+    }
+
+    logout() {
+        localStorage.removeItem(this.tokenKey);
+        this.isLoggedIn$.next(false);
     }
 
     signup(credentials: SignupData): Observable<any> {
-        return this.http.post(`${this.apiUrl}/signup`, credentials);
+        return this.http.post(`${this.apiUrl}/register`, credentials);
     }
 
     forgotPassword(email: string): Observable<any> {
