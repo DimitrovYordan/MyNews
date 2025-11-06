d5efa3101ed06e2a250cb2074cb1af953ddd1228
Yordan <y.dimitrov84@gmail.com>
2025-11-06 13:27:43 +0200

    Change in news-list, environments.

--- MyNews-ui/src/environments/environment.prod.ts
+++ MyNews-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shortglobenews-api-aqe9egg4fvfnehhf.westeurope-01.azurewebsites.net'
+  apiUrl: 'https://api.shortglobenews.com'
 };
