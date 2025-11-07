b11f27f32e2d55692d02b480f802ccd31b028bdf
Yordan <y.dimitrov84@gmail.com>
2025-11-07 22:14:44 +0200

    Change settings.

--- MyNews.Api/Program.cs
+++ MyNews.Api/Program.cs
@@ -159,15 +159,6 @@ app.Use(async (context, next) =>
     }
 });
 
-if (app.Environment.IsProduction())
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        db.Database.Migrate();
-    }
-}
-
 app.UseRouting();
 
 app.UseCors("AllowAngular");
